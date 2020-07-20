f7b57b29ce3981f84f2582d9a74bd9955d211f7c
Ted Moore <ted@tedmooremusic.com>
2020-07-19 20:01:23 -0600

    added fluidDataSet_to_csv file -- not thoroughly tested yet

--- csv_to_FluidDataSet.py
+++ csv_to_FluidDataSet.py
@@ -2,7 +2,7 @@
 Ted Moore
 ted@tedmooremusic.com
 www.tedmooremusic.com
-July 18, 2020
+July 19, 2020
 
 Take in a CSV file of data and create a json file that is appropriately formatted for loading into a FluidDataSet (www.flucoma.org)
 """
@@ -13,34 +13,19 @@ import argparse
 import numpy as np
 import os
 
-def csv_to_FluidDataSet(input_path,start_index,end_index):
+def csv_to_FluidDataSet(csv_data,indices):
 
-    csv_data = []
-
-    with open(args.input) as csv_file:
-        reader = csv.reader(csv_file)
-        for row in reader:
-            csv_data.append(np.array([float(val) for val in row]))
-
-    csv_data = np.array(csv_data)
-
-    if args.start_index == None:
-        start_index = 0
-    else:
-        start_index = args.start_index
-
-    if args.end_index == None:
-        end_index = len(csv_data[0]) - 1
-    else:
-        end_index = args.end_index
-    
     json_dict = {}
 
-    json_dict['cols'] = (end_index - start_index) + 1
+    json_dict['cols'] = len(indices)
 
-    selected_data = csv_data[:,start_index:end_index+1]
-
-    selected_data = selected_data.tolist()
+    selected_data = []
+    
+    for row in csv_data:
+        frame = []
+        for index in indices:
+            frame.append(row[index])
+        selected_data.append(frame)
 
     json_data = {}
 
@@ -57,14 +42,44 @@ if __name__ == '__main__':
     parser.add_argument("--start-index",action='store',dest='start_index',type=int)
     parser.add_argument("--end-index",action='store',dest='end_index',type=int)
     parser.add_argument("--file-suffix",action='store',dest='file_suffix',type=str)
+    parser.add_argument("--specify-indices",action='store',dest='specified_indices',nargs='+',type=int)
     args = parser.parse_args()
 
+    
+    csv_data = []
+
+    with open(args.input) as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            csv_data.append(np.array([float(val) for val in row]))
+
+    csv_data = np.array(csv_data)
+
+    if args.specified_indices == None:
+        start_index = None
+        end_index = None
+        
+        if args.start_index == None:
+            start_index = 0
+        else:
+            start_index = args.start_index
+
+        if args.end_index == None:
+            end_index = len(csv_data[0]) - 1
+        else:
+            end_index = args.end_index
+
+        indices = range(start_index,end_index+1)
+    else:
+        indices = args.specified_indices
+
+    json_dict = csv_to_FluidDataSet(csv_data,indices)
+    
     file_suffix = ''
+    
     if args.file_suffix != None:
         file_suffix = args.file_suffix
     
-    json_dict = csv_to_FluidDataSet(args.input,args.start_index,args.end_index)
-
     new_path = os.path.splitext(args.input)[0] + '_' + file_suffix + '.json'
 
     with open(new_path,"w") as json_file:
---
+++ fluidDataSet_to_csv.py
@@ -0,0 +1,32 @@
+import csv
+import json
+import argparse
+import numpy as np
+import os
+
+def fluidDataSet_to_csv(json_path):
+    data = None
+    with open(json_path) as f:
+        data = json.load(f)
+    csv_data = []
+    for key in data['data']:
+        row = []
+        row.append(key)
+        row.extend(data['data'][key])
+        csv_data.append(row)
+
+    return csv_data
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input",action='store',dest='input',type=str,required=True)
+    parser.add_argument("--file-suffix",action='store',dest='file_suffix',type=str)
+    args = parser.parse_args()
+
+    csv_data = fluidDataSet_to_csv(args.input)
+
+    new_path = os.path.splitext(args.input)[0] + '_' + args.file_suffix + '.csv'
+
+    with open(new_path, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(csv_data)
